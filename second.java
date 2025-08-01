3355b9c88132fa8fb1ade9ee284e6ae48f18d4f2
unknown <gaikwadswaraj12@gmail.com>
2025-08-01 17:47:31 +0530

    new Branch Update

--- second.java
+++ second.java
@@ -2,6 +2,7 @@ public class first {
     public static void main(String args[]) {
         
         system.out.println(x:"Hello From Second class!");
+        system.out.println(x:"Hello From new Branch!");
         
     }
 }
